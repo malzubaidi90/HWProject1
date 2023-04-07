17cebdd30ccc77430f9246272742289e11c85ee1
malzu <malzu@10.0.0.198>
2023-04-06 20:25:33 -0400

    "Noor knows the best restaurants in DMV

---
+++ src/Mohammed/Test.java
@@ -0,0 +1,12 @@
+package Mohammed;
+
+public class Test {
+public static void main (String args []) {
+	
+	System.out.println("Mohammed is the BEST");
+	System.out.println("Of course");
+	
+	
+	
+}
+}
